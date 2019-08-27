b5e6596f21946909e5b99d5767af3231ba00b4e3
MorozovAP <andre15@inbox.ru>
2019-08-27 14:43:17 +0400

    Updates

---
+++ precache-manifest.8bd5ec527ae90e7666ef99a2f3663f8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d162338be6cf55322e46788c87cf9f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af5abe26332d0f7ad9cd",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "94198b1ac348d9cd1b17",
+    "url": "/static/css/main.80f5aac0.chunk.css"
+  },
+  {
+    "revision": "af5abe26332d0f7ad9cd",
+    "url": "/static/js/2.f762c417.chunk.js"
+  },
+  {
+    "revision": "94198b1ac348d9cd1b17",
+    "url": "/static/js/main.cb749990.chunk.js"
+  },
+  {
+    "revision": "7ed22f874241ef804bd9",
+    "url": "/static/js/runtime~main.fe64e12d.js"
+  },
+  {
+    "revision": "ac2243d7bed17ed8bc3ee8d71b408107",
+    "url": "/static/media/me.ac2243d7.jpg"
+  }
+]);
